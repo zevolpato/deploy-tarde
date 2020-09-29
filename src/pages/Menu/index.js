39ea50d088c9b78e1aba4b22aedac164a2959bec
zevolpato <jose.avjunior@sp.senac.br>
2020-09-29 18:50:25 -0300

    commit

--- src/pages/Menu/index.js
+++ src/pages/Menu/index.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaLaptop } from 'react-icons/fa';
 
 import './styles.css';
 
 export default function Menu() {
   return (
       <>
-      <nav>
+      <section>
+          <span><FaLaptop size={150} /></span>
+          
+          <span className="style-span-ul">
           <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/main">Main</Link></li>
-              <li><Link to="/quemsomos">Quem somos...</Link></li>
-              <li><Link to="/ajuda">Ajuda</Link></li>
-            
+              <li><Link to="/" className="style-link">Home</Link></li>
+              <li><Link to="/main" className="style-link">Main</Link></li>
+              <li><Link to="/quemsomos" className="style-link">Quem somos...</Link></li>
+              <li><Link to="/ajuda" className="style-link">Ajuda</Link></li>
           </ul>
-      </nav>
-      <hr />
+          
+          <form>
+          <input placeholder="Sua Id"/>
+          <button className="style-button" type="submit">Entrar</button>  
+          </form>
+          </span>
+      </section>
+     
+   
       </>
     );
 }
